fd8553bb734692e7504075e37e296a82dfca68f7
Furkan Adıgüzel <101877734+furkanadiiguzel@users.noreply.github.com>
2023-10-17 21:50:41 +0300

    Update http.go

--- internal/http/http.go
+++ internal/http/http.go
@@ -1,1 +1,24 @@
 package http
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"mqtt"  //from mqtt.go
+)
+
+func setupHTTPServer() *gin.Engine {
+	r := gin.Default()
+
+	r.POST("/api/movement", func(c *gin.Context) {
+		movementData, err := c.GetRawData()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read request body"})
+			return
+		}
+
+		mqtt.Publish(movementData)
+		c.String(http.StatusOK, "Movement request sent to MQTT")
+	})
+
+	return r
+}
